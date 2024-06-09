a9b243d86d15c2bdead53a64d4c6b1488c4e5727
burt reynolds <jnelson4004@gmail.com>
2024-06-09 16:15:05 -0500

    mighta just nailed the back end

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { startApolloServer} = require("apollo-server-express");
 const path = require('path');
+const {typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const db = require('./config/connection');
 const routes = require('./routes');
 
@@ -10,12 +13,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+async function startServer(typeDefs, resolvers) {
+  const server = new startApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
 
-app.use(routes);
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+};
+
+startServer(typeDefs, resolvers);
